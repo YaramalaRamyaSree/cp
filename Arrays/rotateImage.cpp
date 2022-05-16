155e500c74061d0d54946f78bd48b8f11a0063c7
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-05-16 14:02:16 +0530

    Create rotateImage.cpp

---
+++ Arrays/rotateImage.cpp
@@ -0,0 +1,16 @@
+/*
+leetcode link - https://leetcode.com/problems/rotate-image/
+*/
+
+// 90 degree clockwise rotation =transpose + reverse every row
+
+    void rotate(vector<vector<int>>& matrix) {
+        
+        for(int i=0;i<matrix.size();i++)
+            for(int j=0;j<i;j++)
+                swap(matrix[i][j],matrix[j][i]);
+        
+        for(int i=0;i<matrix.size();i++)
+            reverse(matrix[i].begin(),matrix[i].end());
+        
+    }
