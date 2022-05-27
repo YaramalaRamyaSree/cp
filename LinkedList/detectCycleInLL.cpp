cd80da6756b3553c6d88e202e4989e37494265d5
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-05-27 09:26:20 +0530

    Create detectCycleInLL.cpp

---
+++ LinkedList/detectCycleInLL.cpp
@@ -0,0 +1,23 @@
+/*
+leetcode link - https://leetcode.com/problems/linked-list-cycle/
+*/
+
+    //definetely fast pointer will catch up slow is intution T.C=O(N),S.C=O(1)
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL) return false;
+        ListNode*slow=head,*fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast)
+                return true;
+        }
+        return false;
+    }
+    
+    /*Another approch in datastructure of Node add flag also every time u traverse make flag 1 and 
+      traverse when ever u see alredy node's flag as 1 return true else false
+    
+      Approch -without changing datastructure of Node also we can make use  flag concept how??
+      by pointing traversed nodes to some temp value ,when ever u see a node pointing to temp we return true 
+      */
