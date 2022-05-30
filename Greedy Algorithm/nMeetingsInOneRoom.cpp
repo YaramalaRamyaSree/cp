8a19437d7262022b42c5d587463e7aff96c946e9
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-05-30 18:20:00 +0530

    Create nMeetingsInOneRoom.cpp

---
+++ Greedy Algorithm/nMeetingsInOneRoom.cpp
@@ -0,0 +1,30 @@
+/*
+gfg link - https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1#
+*/
+
+   struct meet{
+        int start;
+        int end;
+    };
+    bool static comparator(struct meet m1,meet m2){
+        return (m1.end<m2.end);
+    }
+    int maxMeetings(int start[], int end[], int n)
+    {
+       struct meet meeting[n];
+       for(int i=0;i<n;i++){
+          meeting[i].start=start[i];
+          meeting[i].end=end[i];
+       }
+       sort(meeting,meeting+n,comparator);
+       int count=1;
+       int e=meeting[0].end;
+       for(int i=1;i<n;i++){
+           if(meeting[i].start>e)
+           {
+               count++;
+               e=meeting[i].end;
+           }
+       }
+       return count;
+    }
