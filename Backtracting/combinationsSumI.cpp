c8035ce7e2a5b746209b1ff9cf554517e355f0da
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-06-08 00:50:54 +0530

    Update combinationsSumI.cpp

--- Backtracting/combinationsSumI.cpp
+++ Backtracting/combinationsSumI.cpp
@@ -1,8 +1,10 @@
 /*
-leetcode link - https://leetcode.com/problems/combination-sum/submissions/
+leetcode link - https://leetcode.com/problems/combination-sum/
 */
+
    //T.C=(2^t *k) =k is avg len of ds pushed into ans,2^t calls  we may end doing in worst case , t may be unpredictable
    //S.C=O(k*x),x is no of combinations in ans and k is avg length + call stack 
+
     void findCombinations(int i,int target,vector<int>& arr, int n,vector<int>&ds, vector<vector<int>>&ans){
         if(i==n){
             if(target==0){
@@ -17,6 +19,7 @@ leetcode link - https://leetcode.com/problems/combination-sum/submissions/
         }
         findCombinations(i+1,target,arr,n,ds,ans);
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>ds;
