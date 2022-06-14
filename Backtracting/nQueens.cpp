190adec8a0228cb8a13277710d9cdbf086c23bb7
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-06-14 15:02:29 +0530

    Update nQueens.cpp

--- Backtracting/nQueens.cpp
+++ Backtracting/nQueens.cpp
@@ -1,40 +1,8 @@
 /*
 leetcode link - https://leetcode.com/problems/n-queens/
-//T.C=O(N!*N) T(n)=n+n*T(n-1)
-    //S.C=O(n^2)
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>ans;
-        vector<string>board(n);
-        string s(n,'.');
-        for(int i=0;i<n;i++) board[i]=s;
-        vector<int> leftrow(n,0),upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
-        find(0,n,board,ans,leftrow,upperDiagonal,lowerDiagonal);
-        return ans;
-    }
-    
-    void find(int col,int n,vector<string>&board,vector<vector<string>>&ans,vector<int>&leftrow,vector<int>&upperDiagonal,vector<int>&lowerDiagonal){
-        if(col==n){
-            ans.push_back(board);
-        }
-        
-        for(int row=0;row<n;row++){
-            if(leftrow[row]!=1&&upperDiagonal[n-1+(col-row)]!=1&&lowerDiagonal[row+col]!=1){
-                leftrow[row]=1;
-                upperDiagonal[n-1+(col-row)]=1;
-                lowerDiagonal[row+col]=1;
-                board[row][col]='Q';
-                find(col+1,n,board,ans,leftrow,upperDiagonal,lowerDiagonal);
-                board[row][col]='.';
-                leftrow[row]=0;
-                upperDiagonal[n-1+(col-row)]=0;
-                lowerDiagonal[row+col]=0;
-            }
-        }
-    }
-    
 */
 
-    //T.C=O(N!*N) T(n)=n^2+n*T(n-1)
+    //T.C=O(N!*N) T(n)=n+n*T(n-1)
     //S.C=O(n^2)
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
@@ -45,7 +13,7 @@ leetcode link - https://leetcode.com/problems/n-queens/
         return ans;
     }
     
-    void find(int col,int n,vector<string>&board,vector<vector<string>>&ans){
+     void find(int col,int n,vector<string>&board,vector<vector<string>>&ans){
         if(col==n){
             ans.push_back(board);
         }
@@ -78,8 +46,9 @@ leetcode link - https://leetcode.com/problems/n-queens/
         return true;
     }
 
-//OPTIMISING isSafe function  from o(n) to o(1) by hashing -extra space added of 3O(n). 
-//T.C=O(N!*N) T(n)=n+n*T(n-1)
+
+    //OPTIMISING isSafe function  from o(n) to o(1) by hashing -extra space added of 3O(n). 
+    //T.C=O(N!*N) T(n)=n+n*T(n-1)
     //S.C=O(n^2)
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
