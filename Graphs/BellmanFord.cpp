193044cc1dae0fb319dfbbd9f3f05d04d2a96b7a
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-07-03 23:03:20 +0530

    Update BellmanFord.cpp

--- Graphs/BellmanFord.cpp
+++ Graphs/BellmanFord.cpp
@@ -2,7 +2,9 @@
 gfg link - https://practice.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/0/?fbclid=IwAR2_lL0T84DnciLyzMTQuVTMBOi82nTWNLuXjUgahnrtBgkphKiYk6xcyJU
 */
 
-    //T.C=O(E*V)S.C=O(N)
+    //T.C=O(E*V)S.C=O(V)
+//If we want to detect negative cycle we can by running loop again  but in question mentioned no negative cycle 
+//Bellmanford doesn't work on negative cycles
     vector <int> bellman_ford(int V, vector<vector<int>> adj, int S) {
         vector<int>dist(V,100000000);
         dist[S]=0;
