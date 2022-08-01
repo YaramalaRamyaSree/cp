c0e5aec5d8a1bbcb3ea80e137599ee535f3d40f9
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-08-02 00:35:18 +0530

    Update EggDropping.cpp

--- DP/EggDropping.cpp
+++ DP/EggDropping.cpp
@@ -2,6 +2,33 @@
 gfg link - https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle-1587115620/1
 */
 
+/*IDEA-
+1.if we have 1 egg k floors we need k steps  
+2.Here we need to find minimum of (maximum steps in worst case) 
+3.We don't care about egg (it may or may not break at all ) we just need to find min of max steps to tell whether egg breaks at f floor and f is not known to us 
+IDEA is to find at every step possibilities are egg break and not break (IF  at ith floor egg 
+                                                                          BREAK -  we will have n-1 eggs and i-1 floors f(n-1,i-1
+                                                                         NOT BREAK - we will have to find problem of n eggs and k-i floors f(n,k-i)  ).
+*/
+
+//Recursion -T.C=O(2^(n*(k^2))) S.C=O(n)
+    int f(int n,int k){
+        if(n==1) return k;
+        if(k==0) return 0;
+        int mini=INT_MAX;
+        for(int i=1;i<=k;i++){
+            int Break=f(n-1,i-1,dp);
+            int notBreak=f(n,k-i,dp);
+            int maxi =max(Break,notBreak);
+            mini=min(mini,1+maxi);
+        }
+        return mini;
+    }
+    int eggDrop(int n, int k) 
+    {
+        return f(n,k);
+    }
+
 //Memoization -T.C=O(n*(k^2)) S.C=O(n*k)+O(n)
     int f(int n,int k,vector<vector<int>>&dp){
         if(n==1) return k;
