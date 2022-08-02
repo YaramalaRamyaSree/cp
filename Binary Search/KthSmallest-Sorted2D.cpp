67c7350b0a959d18df7e9b589bd8a7cd84b6a726
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-08-02 16:57:00 +0530

    Update KthSmallest-Sorted2D.cpp

--- Binary Search/KthSmallest-Sorted2D.cpp
+++ Binary Search/KthSmallest-Sorted2D.cpp
@@ -64,3 +64,18 @@ LEETCODE LINK - https://leetcode.com/problems/kth-smallest-element-in-a-sorted-m
         
       return left; //return right+1; or return ans;
     }
+
+
+//with max heap T.C=O((N*M)logK) S.C=O(K)
+    int kthSmallest(vector<vector<int>>& matrix, int k) { 
+        priority_queue<int>pq;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                pq.push(matrix[i][j]);
+                if(pq.size()>k){
+                    pq.pop();
+                }
+            }
+        }
+       return pq.top();  
+    }
