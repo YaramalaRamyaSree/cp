fab1d6c8321acedbd0593842a0d0fe66532d7460
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-08-06 21:41:16 +0530

    Create poorPigs.cpp

---
+++ Math/poorPigs.cpp
@@ -0,0 +1,17 @@
+/*
+leetcode link - https://leetcode.com/problems/poor-pigs/
+*/
+
+//this can even be sloved using recursion,dp 
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int x= minutesToTest/minutesToDie;
+        int pigs=0;
+        /*
+        //this will exceed time
+        while(pow(x,pigs)<buckets){
+            pigs++;
+        }
+        */
+        pigs=ceil(log(buckets)/log(x+1));
+        return pigs;
+    }
