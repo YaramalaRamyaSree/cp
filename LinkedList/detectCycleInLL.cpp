ef11b3822daae1cbcb2163ebd5f30db9944a474e
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-09-05 06:44:23 +0530

    Update detectCycleInLL.cpp

--- LinkedList/detectCycleInLL.cpp
+++ LinkedList/detectCycleInLL.cpp
@@ -15,9 +15,23 @@ leetcode link - https://leetcode.com/problems/linked-list-cycle/
         return false;
     }
     
-    /*Another approch in datastructure of Node add flag also every time u traverse make flag 1 and 
+    /*Another approch  2 in datastructure of Node add flag also every time u traverse make flag 1 and 
       traverse when ever u see alredy node's flag as 1 return true else false
-    
-      Approch -without changing datastructure of Node also we can make use  flag concept how??
-      by pointing traversed nodes to some temp value ,when ever u see a node pointing to temp we return true 
       */
+
+//Approch 3 -without changing datastructure of Node also we can make use  flag concept how??
+// by pointing traversed nodes to some temp value ,when ever u see a node pointing to temp we return true 
+    bool detectLoop(Node* head)
+    {
+        Node* point=new Node(-1);
+        Node* temp=head;
+        if(!head || !head->next)
+            return false;
+        while(temp){
+            if(temp->next==point) return true;
+            Node* temp2 =temp->next;
+            temp->next=point;
+            temp=temp2;
+        }
+        return false;
+    }
