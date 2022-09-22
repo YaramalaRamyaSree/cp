79b8b4538d8de421d90bd97aaa52c813ce606141
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-09-23 00:48:49 +0530

    Update ParenthesisChecker.cpp

--- Strings/ParenthesisChecker.cpp
+++ Strings/ParenthesisChecker.cpp
@@ -25,3 +25,35 @@ gfg link - https://practice.geeksforgeeks.org/problems/parenthesis-checker2744/1
         if(!s.empty()) return false;
         return true;
     }
+
+//T.C=O(N^2) S.C=O(1)
+
+    bool isValid(string s) {
+        int top=-1;
+        unordered_map<char,char>m;
+        m[')']='(';
+        m['}']='{';
+        m[']']='[';
+        for(int i=0;i<s.size();i++){
+            if(m.find(s[i])==m.end())
+                top=i;
+            else{
+                if(top==-1 || m[s[i]]!=s[top])
+                    return 0;
+                top=find_top(s,top-1,m);
+            }
+        }
+        return top==-1;
+    }
+    
+    int find_top(string s,int index,unordered_map<char,char>m){
+        int right=0;
+        while(index>=0){
+            if(m.find(s[index])!=m.end())
+                right++;
+            else right--;
+            if(right<0)return index;
+            index--;
+        }
+        return -1;
+    }
