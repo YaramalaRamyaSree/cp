e48eaf886e1a9b6fd3b01ef74bea856a74c71717
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-09-23 00:16:01 +0530

    Update LongestValidParentheses.cpp

--- Strings/LongestValidParentheses.cpp
+++ Strings/LongestValidParentheses.cpp
@@ -23,3 +23,48 @@ gfg link - https://practice.geeksforgeeks.org/problems/longest-valid-parentheses
     }
 
 //T.C=O(N) S.C=O(1)
+
+public:
+    int maxLength(string s){
+        int open = 0 , close = 0;
+        int maxi = 0;
+        
+        // 0 -- n
+        for(int i=0;i<s.length();i++){
+            char c = s[i];
+            if(c == '('){
+                open++;
+            }
+            else{
+                close++;
+            }
+            
+            if(open == close){
+                int len = open + close;
+                maxi = max(maxi,len);
+            }
+            else if(close > open ){
+                open = close = 0;
+            }
+        }
+         open = close = 0;
+        // n -- 0
+        for(int i= s.length()-1; i>=0 ;i--){
+            char c = s[i];
+            if(c == '('){
+                open++;
+            }
+            else{
+                close++;
+            }
+            
+            if(open == close){
+                int len = open + close;
+                maxi = max(maxi,len);
+            }
+            else if(open > close ){
+                open = close = 0;
+            }
+        }
+        return maxi;
+    }
