b5d63d4221d5d5b14d4a9a7c511060bc470e1064
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-10-08 02:28:36 +0530

    Create DetermineIfTwoTreesAreIdentical.cpp

---
+++ BinaryTree/DetermineIfTwoTreesAreIdentical.cpp
@@ -0,0 +1,10 @@
+/*
+gfg link - https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+*/
+
+
+ bool isIdentical(Node *r1, Node *r2)
+    {
+        if(r1==NULL) return r1==r2;
+        return r1->data==r2->data&&isIdentical(r1->left,r2->left)&&isIdentical(r1->right,r2->right);
+    }
