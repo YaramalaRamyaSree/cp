5d85c370d588a3529c74647f1f72d3a5e6a69681
YaramalaRamyaSree <66723172+YaramalaRamyaSree@users.noreply.github.com>
2022-10-09 14:36:02 +0530

    Create MaximumWidthOfBT.cpp

---
+++ BinaryTree/MaximumWidthOfBT.cpp
@@ -0,0 +1,29 @@
+/*
+leetcode link - https://leetcode.com/problems/maximum-width-of-binary-tree/
+*/
+
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        int ans=0;
+        while(!q.empty()){
+            int size=q.size();
+            int minn=q.front().second;
+            int first,last;
+                for(int i=0;i<size;i++){
+                    long int currid=q.front().second-minn;
+                    TreeNode* node = q.front().first;
+                    q.pop();
+                    if(i==0) first =currid;
+                    if(i==size-1) last =currid;
+                    if(node->left){
+                        q.push({node->left,currid*2+1});
+                    }
+                    if(node->right){
+                        q.push({node->right,currid*2+2});
+                    }
+                }
+            ans=max(ans,last-first+1);
+        }
+        return ans;
+    }
